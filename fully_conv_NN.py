ba1e046be6cf6197fc729d001906a3b83cece2ff
Tanukimong <tlsgusghq@me.com>
2019-10-14 03:03:01 +0900

    결과 값의 차원 일치

--- fully_conv_NN.py
+++ fully_conv_NN.py
@@ -24,14 +24,16 @@ from keras.preprocessing.image import ImageDataGenerator
 train_images,labels, _ = pickle.load(open("tanuki_train.p", "rb" ))
 
 # Make into arrays as the neural network wants these
-train_images = np.array(train_images)
-labels = np.array(labels)
+labels = np.array[..., np.newaxis]
+
+print("Train_imgs is {}, labels is {}".format(train_images.shape, labels.shape))
 
 # Normalize labels - training images get normalized to start in the network
 labels = labels / 255
 
 # Shuffle images along with their labels, then split into training/validation sets
 train_images, labels = shuffle(train_images, labels)
+
 # Test size may be 10% or 20%
 X_train, X_val, y_train, y_val = train_test_split(train_images, labels, test_size=0.1)
 
@@ -140,10 +142,10 @@ model.fit_generator(datagen.flow(X_train, y_train, batch_size=batch_size), sampl
 
 # Save model architecture and weights
 model_json = model.to_json()
-with open("full_CNN_model.json", "w") as json_file:
+with open("tanuki_model.json", "w") as json_file:
     json_file.write(model_json)
 
-model.save_weights('full_CNN_model.h5')
+model.save_weights('tanuki_model.h5')
 
 # Show summary of model
 model.summary()
