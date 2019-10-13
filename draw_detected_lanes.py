050c959745d44affdcb115fb75cdd0025ddc1608
Tanukimong <tlsgusghq@me.com>
2019-10-14 04:08:02 +0900

    draw lanes 사용 가능하게 수정 인자는 입력파일 출력파일 순

--- draw_detected_lanes.py
+++ draw_detected_lanes.py
@@ -1,16 +1,22 @@
 import numpy as np
 import cv2
-from scipy.misc import imresize
+from PIL.Image import fromarray
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
 from keras.models import model_from_json
+import sys
+import warnings
+warnings.filterwarnings(action='ignore') # 귀찮은 경고 감추기
 
 # Load Keras model
-json_file = open('full_CNN_model.json', 'r')
+json_file = open('tanuki_model.json', 'r')
 json_model = json_file.read()
 json_file.close()
 model = model_from_json(json_model)
-model.load_weights('full_CNN_model.h5')
+model.load_weights('tanuki_model.h5')
+
+scaler = 6
+resized_shape = (1640//scaler, 590//scaler)
 
 # Class to average lanes with
 class Lanes():
@@ -24,10 +30,9 @@ def road_lines(image):
     recreates an RGB image of a lane and merges with the
     original road image.
     """
-
     # Get image ready for feeding into model
-    small_img = imresize(image, (80, 160, 3))
-    small_img = np.array(small_img)
+    small_img = fromarray(image).resize(resized_shape)
+    small_img = np.array(small_img,dtype="uint8")
     small_img = small_img[None,:,:,:]
 
     # Make prediction with neural network (un-normalize value by multiplying by 255)
@@ -43,11 +48,14 @@ def road_lines(image):
     lanes.avg_fit = np.mean(np.array([i for i in lanes.recent_fit]), axis = 0)
 
     # Generate fake R & B color dimensions, stack with G
-    blanks = np.zeros_like(lanes.avg_fit).astype(np.uint8)
+    blanks = np.zeros_like(lanes.avg_fit)
     lane_drawn = np.dstack((blanks, lanes.avg_fit, blanks))
+    lane_drawn = lane_drawn.astype("uint8")
 
     # Re-size to match the original image
-    lane_image = imresize(lane_drawn, (720, 1280, 3))
+    lane_image = fromarray(lane_drawn)
+    lane_image = lane_image.resize((1280, 720))
+    lane_image = np.asarray(lane_image,dtype="uint8")
 
     # Merge the lane drawing onto the original image
     result = cv2.addWeighted(image, 1, lane_image, 1, 0)
@@ -57,10 +65,10 @@ def road_lines(image):
 lanes = Lanes()
 
 # Where to save the output video
-vid_output = 'proj_reg_vid.mp4'
+vid_output = sys.argv[1]
 
 # Location of the input video
-clip1 = VideoFileClip("project_video.mp4")
+clip1 = VideoFileClip(sys.argv[2])
 
 vid_clip = clip1.fl_image(road_lines)
 vid_clip.write_videofile(vid_output, audio=False)
