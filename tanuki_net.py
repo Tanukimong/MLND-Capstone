cbfade962fd92732d6b70c13f10e2753f6954c0c
Tanukimong <tlsgusghq@me.com>
2019-10-16 09:22:35 +0900

    tanuki_net 원래 구조와 거의 동일하게 변경

--- tanuki_net.py
+++ tanuki_net.py
@@ -7,51 +7,51 @@ from keras import backend as keras
 def tanuki_net(input_shape, pool_size):
     inputs = Input(input_shape) 
     batch = BatchNormalization()(inputs) #0 
-    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(batch)
+    conv1 = Conv2D(60, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(batch)
     drop1 = Dropout(0.2)(conv1)#2
-    conv1 = Conv2D(32, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop1)
+    conv1 = Conv2D(50, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop1)
     drop1 = Dropout(0.2)(conv1)#4
     pool1 = MaxPooling2D(pool_size=(2, 2))(drop1)
 
     batch = BatchNormalization()(pool1)
-    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(batch)
+    conv2 = Conv2D(40, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(batch)
     drop2 = Dropout(0.2)(conv2)#7
-    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop2)
+    conv2 = Conv2D(30, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop2)
     drop2 = Dropout(0.2)(conv2)
-    conv2 = Conv2D(64, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop2)
+    conv2 = Conv2D(20, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop2)
     drop2 = Dropout(0.2)(conv2)#11
     pool2 = MaxPooling2D(pool_size=(2, 2))(drop2)
 
     batch = BatchNormalization()(pool2)
-    conv3 = Conv2D(128, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(batch)
+    conv3 = Conv2D(10, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(batch)
     drop3_1 = Dropout(0.2)(conv3)#14
-    conv3 = Conv2D(128, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop3_1)
+    conv3 = Conv2D(5, 3, activation = 'relu', padding = 'valid', kernel_initializer = 'he_normal')(drop3_1)
     drop3 = Dropout(0.2)(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(drop3)
 
     up1 = UpSampling2D(size=pool_size)(pool3)
     batch = BatchNormalization()(up1)
-    deconv1 = Conv2DTranspose(128, 3, strides=(1, 1), padding='valid', activation = 'relu')(batch)
+    deconv1 = Conv2DTranspose(10, 3, strides=(1, 1), padding='valid', activation = 'relu')(batch)
     merge1 = concatenate([drop3_1,deconv1], axis = 3)
     drop4 = Dropout(0.2)(merge1)
-    deconv1 = Conv2DTranspose(128, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop4)
+    deconv1 = Conv2DTranspose(20, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop4)
     drop4 = Dropout(0.2)(deconv1)
 
     up2 = UpSampling2D(size=pool_size)(drop4)
     batch = BatchNormalization()(up2)
-    deconv2 = Conv2DTranspose(64, 3, strides=(1, 1), padding='valid', activation = 'relu')(batch)
+    deconv2 = Conv2DTranspose(30, 3, strides=(1, 1), padding='valid', activation = 'relu')(batch)
 
     drop5 = Dropout(0.2)(deconv2)
-    deconv2 = Conv2DTranspose(64, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop5)
+    deconv2 = Conv2DTranspose(40, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop5)
     drop5 = Dropout(0.2)(deconv2)
-    deconv2 = Conv2DTranspose(64, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop5)
+    deconv2 = Conv2DTranspose(50, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop5)
     drop5 = Dropout(0.2)(deconv2)
 
     up3 = UpSampling2D(size=pool_size)(drop5)
     batch = BatchNormalization()(up3)
-    deconv3 = Conv2DTranspose(32, 3, strides=(1, 1), padding='valid', activation = 'relu')(batch)
+    deconv3 = Conv2DTranspose(60, 3, strides=(1, 1), padding='valid', activation = 'relu')(batch)
     drop6 = Dropout(0.2)(deconv3)
-    deconv3 = Conv2DTranspose(32, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop6)
+    deconv3 = Conv2DTranspose(70, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop6)
     deconv4 = Conv2DTranspose(1, 3, strides=(1, 1), padding='valid', activation = 'relu')(drop6)
 
     model = Model(input = inputs, output = deconv4)
